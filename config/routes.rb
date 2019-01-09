f198c37b38e042a9c471a8bcfac4fd4206e028ab
NasuYayoi <ynynyn1014@gmail.com>
2019-01-09 13:47:24 +0900

    Edit route.rb
    各コンテンツページに合わせてルーティングも修正。

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,25 @@ Rails.application.routes.draw do
     resources :memos
   end
 
+  #contents
+  # infra
   get 'contents/what_infra'
-  get 'contents/linux'
   get 'contents/server'
   get 'contents/network'
+  get 'contents/os'
+  get 'contents/storage'
   get 'contents/virtualization'
+  get 'contents/cloud'
+
+  # linux
+  get 'contents/linux'
+  get 'contents/env_const'
+  get 'contents/command'
+  get 'contents/ssh'
+
+  # other
+  get 'contents/lightning_talk'
+  get 'contents/ses_etc'
   get 'contents/experience'
 
 end
